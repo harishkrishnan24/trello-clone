717c4dad7424eef5a7cd53fcddca0a479fe101ba
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-09-03 19:51:34 +0530

    Add new input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 
 import { AppContainer } from "./styles";
 import { Column } from "./Column";
 import { Card } from "./Card";
+import { AddNewItem } from "./AddNewItem";
 
 function App() {
-  return (
-    <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold"></Card>
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript"></Card>
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing"></Card>
-      </Column>
-    </AppContainer>
-  );
+	return (
+		<AppContainer>
+			<Column text='To Do'>
+				<Card text='Generate app scaffold'></Card>
+			</Column>
+			<Column text='In Progress'>
+				<Card text='Learn Typescript'></Card>
+			</Column>
+			<Column text='Done'>
+				<Card text='Begin to use static typing'></Card>
+			</Column>
+			<AddNewItem toggleButtonText='+ Add another list' onAdd={console.log} />
+		</AppContainer>
+	);
 }
 
 export default App;
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 import { ColumnContainer, ColumnTitle } from "./styles";
+import { AddNewItem } from "./AddNewItem";
 
 interface ColumnProps {
-    text: string;
+	text: string;
 }
 
-export const Column = ({text, children}: React.PropsWithChildren<ColumnProps>) => {
-    return <ColumnContainer>
-        <ColumnTitle>{text}</ColumnTitle>
-        {children}
-    </ColumnContainer>
-}
+export const Column = ({
+	text,
+	children,
+}: React.PropsWithChildren<ColumnProps>) => {
+	return (
+		<ColumnContainer>
+			<ColumnTitle>{text}</ColumnTitle>
+			{children}
+			<AddNewItem
+				toggleButtonText='+ Add another task'
+				onAdd={console.log}
+				dark
+			/>
+		</ColumnContainer>
+	);
+};
